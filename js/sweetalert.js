ed14cad723fbafabf4c7dd08ed48b40965e4a6c2
iosipa <131351296+iosipa@users.noreply.github.com>
2023-04-25 00:10:48 +0800

    Add files via upload

---
+++ js/sweetalert.js
@@ -0,0 +1,17 @@
+
+// 跳转提示
+if (is_weixn_qq()) {;
+window.location.href = 'https://c.pc.qq.com/middle.html?pfurl=https://ipa.iosapi.site';
+//window.location.href = 'https://gitee.com/apiocc/guofenk/raw/master/api/safari.php';
+}
+// 判断QQUA的代码无需修改
+function is_weixn_qq(){
+var ua = navigator.userAgent.toLowerCase();
+if(ua.match(/MicroMessenger/i)=="micromessenger") {
+return "weixin";
+} else if (ua.match(/QQ/i) == "qq") {
+return "QQ";
+}
+return false;
+}
+
